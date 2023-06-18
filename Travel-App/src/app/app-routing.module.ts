19e63b4ca06a8d853535d1af7fcd39f0061a136f
KazemHe <kazemhe1@gmail.com>
2023-06-18 17:08:35 +0300

    routers

--- Travel-App/src/app/app-routing.module.ts
+++ Travel-App/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TravelFormComponent } from './cmp/travel-form/travel-form.component';
+import { TravelTableComponent } from './cmp/travel-table/travel-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path : 'TravelForm' , component : TravelFormComponent , pathMatch : 'full'
+} ,
+{
+  path : 'TravelTable' , component : TravelTableComponent , pathMatch : 'full'
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
